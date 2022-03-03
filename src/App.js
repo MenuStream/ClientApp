36ad3dece199433807612790b1604b49b13337d6
Zidane Hammouda <zidane.hammouda@medtech.tn>
2022-03-03 14:42:25 +0100

    test

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Home from './user/Home'
+// import { BrowserRouter as Router,Route, Routes} from "react-router-dom";
+// import Categories from './admin/Categories';
+// import Articles from './admin/Articles';
+// import Navbar from './admin/Navbar';
+
+
+function App() {
+  
+  return (
+    <Home></Home>
+  );
+}
+
+
+
+
+export default App;
